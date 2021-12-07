5bd1f526b3fe7c115da2992c8fde0af1b07f7fa3
FirstVertex <hughblue@gmail.com>
2021-12-07 12:08:03 -0500

    feat(Digit): wraparound

--- NumberSpinner/Digit.lua
+++ NumberSpinner/Digit.lua
@@ -12,8 +12,11 @@ function Digit.new(Spinner, LayoutOrder, Value)
 
 		Labels = table.create(10);
 		CanvasTweens = table.create(10);
+
+		LabelsOverflow = table.create(10);
+		CanvasTweensOverflow = table.create(10);
 	}
-	local tweenInfo = TweenInfo.new(Spinner.Duration)
+	local tweenInfo = TweenInfo.new(Spinner.Duration, Enum.EasingStyle.Linear)
 
 	local Frame = Instance.new("Frame")
 	Frame.Name = "digit"
@@ -24,26 +27,70 @@ function Digit.new(Spinner, LayoutOrder, Value)
 
 	local Canvas = Instance.new("Frame")
 	Canvas.Name = "canvas"
-	Canvas.Size = UDim2.new(1,0,10,0)
 	Canvas.BackgroundTransparency = 1
 	Canvas.Parent = Frame
 
-	Canvas.Position = UDim2.new(0,0,-d.Value,0)
+	local ySteps = 20
+	local ySize = 1/ySteps
+	Canvas.Size = UDim2.new(1,0,ySteps,0)
+	Canvas.Position = UDim2.new(0,0,-Value/2,0)
 
-	for i=0,9 do
-		local n = Instance.new("TextLabel")
-		n.Name = "n_"..i
-		n.BackgroundTransparency = 1
-		n.TextSize = Spinner.TextSize
-		n.TextColor3 = Spinner.TextColor3
-		n.Font = Spinner.Font
-		n.Text = i
-		n.Size = UDim2.new(1,0,0.1,0)
-		n.Position = UDim2.new(0,0,i*0.1,0)
-		n.Parent = Canvas
-		d.Labels[i] = n
-		d.CanvasTweens[i] = TweenService:Create(Canvas, tweenInfo, {Position = UDim2.new(0,0,-i,0)})
+	-- what's actually displayed on the dial right now
+	local function getDisplayedValue()
+		local currentY = Canvas.Position.Y.Scale
+		if (currentY <= -10) then
+			currentY += 10
+		end
+		return -currentY
 	end
+
+	local function wrapAround()
+		local currentY = Canvas.Position.Y.Scale;
+		if (currentY <= -10) then
+			-- quickly move back to the actual numbers instead of the overflow numbers
+			Canvas.Position = UDim2.new(0,0,currentY+10,0)
+		end
+	end
+	
+	local function createTweens()
+		for j=1,2 do
+			for i=0,9 do
+				local labelY = i + (10*(j-1))
+				local tweens = j == 1 and d.CanvasTweens or d.CanvasTweensOverflow
+				if (tweens[i] ~= nil and tweens[i].tweenCompleted ~= nil) then
+					tweens[i].tweenCompleted:Disconnect()
+				end
+				local tween = TweenService:Create(Canvas, tweenInfo, {Position = UDim2.new(0,0,-labelY,0)})
+				local tweenCompleted = nil
+				if (j == 2) then
+					tweenCompleted = tween.Completed:Connect(wrapAround)
+				end
+				tweens[i] = {
+					tween = tween;
+					tweenCompleted = tweenCompleted;
+				}
+			end
+		end
+	end
+
+	for j=1,2 do
+		local labels = j == 1 and d.Labels or d.LabelsOverflow
+		for i=0,9 do
+			local labelY = i + (10*(j-1))
+			local n = Instance.new("TextLabel")
+			n.Name = "n_"..i
+			n.BackgroundTransparency = 1
+			n.TextSize = Spinner.TextSize
+			n.TextColor3 = Spinner.TextColor3
+			n.Font = Spinner.Font
+			n.Text = i
+			n.Size = UDim2.new(1,0,ySize,0)
+			n.Position = UDim2.new(0,0,labelY/ySteps,0)
+			n.Parent = Canvas
+			labels[i] = n
+		end
+	end
+	createTweens()
 
 	Frame.Parent = Spinner.Frame
 
@@ -72,8 +119,11 @@ function Digit.new(Spinner, LayoutOrder, Value)
 
 			local Success = pcall(function() local x = d.Labels[1][key] end)
 			if Success then
-				for i=0,9 do
-					d.Labels[i][key] = value
+				for j=1,2 do
+					local labels = j == 1 and d.Labels or d.LabelsOverflow
+					for i=0,9 do
+						labels[i][key] = value
+					end
 				end
 				d:Update(key,value)
 			end
@@ -87,19 +137,31 @@ function Digit.new(Spinner, LayoutOrder, Value)
 			Frame:Destroy()
 			table.clear(d)
 			shrinkTween:Destroy()
+			local tweens = d.CanvasTweensOverflow
+			if (tweens ~= nil) then
+				for i=0,9 do
+					if (tweens[i] ~= nil and tweens[i].tweenCompleted ~= nil) then
+						tweens[i].tweenCompleted:Disconnect()
+						tweens[i].tweenCompleted = nil
+					end
+				end
+			end
+
 		end)
 		shrinkTween:Play()
 	end
 
 	function d:Update(Type,Value)
 		if Type == "Duration" then
-			tweenInfo = TweenInfo.new(Value)
-			for i=0,9 do
-				d.CanvasTweens[i] = TweenService:Create(Canvas, tweenInfo, {Position = UDim2.new(0,0,-i,0)})
-			end
+			tweenInfo = TweenInfo.new(Value, Enum.EasingStyle.Linear)
+			createTweens()
 
 		elseif Type == "Value" then
-			d.CanvasTweens[Value]:Play()
+			local visibleValue = getDisplayedValue()
+			-- when updating to a smaller number than what's currently visible, use the overflow tweens
+			-- these will reset the canvas to the visible range when canceled/completed
+			local tweens = Value < visibleValue and d.CanvasTweensOverflow or d.CanvasTweens
+			tweens[Value].tween:Play()
 
 		elseif Type == "TextSize" or Type == "Font" then
 			local Size = TextService:GetTextSize("8", Spinner.TextSize, Spinner.Font, Vector2.new(Spinner.TextSize,Spinner.TextSize))
